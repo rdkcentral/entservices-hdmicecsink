4fc071430a176d41ae42133085d05da74b8f08eb
dkumar798 <dinesh_kumar2@comcast.com>
2026-02-12 01:15:11 +0530

    RDKEMW-13684: Integrate entservices-hdmicecsink to middleware builds (#4)
    * RDKEMW-13684:Integrate entservices-hdmicecsink to middleware builds
    
    * RDKEMW-13684:Integrate entservices-hdmicecsink to middleware builds
    
    * RDKEMW-13684:Integrate entservices-hdmicecsink to middleware builds
    
    * RDKEMW-13684:Integrate entservices-hdmicecsink to middleware builds
    
    * RDKEMW-13684:Integrate entservices-hdmicecsink to middleware builds
    
    * Update CODEOWNERS
    
    ---------
    
    Co-authored-by: DineshkumarJP <dineshitsoft@gmail.com>

---
+++ build_dependencies.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+set -x
+set -e
+##############################
+GITHUB_WORKSPACE="${PWD}"
+ls -la ${GITHUB_WORKSPACE}
+cd ${GITHUB_WORKSPACE}
+
+# # ############################# 
+#1. Install Dependencies and packages
+
+apt update
+apt install -y libcurl4-openssl-dev valgrind lcov clang libsystemd-dev libboost-all-dev curl libunwind-dev libdrm-dev
+pip install jsonref
+
+# Clone the required repositories
+
+
+git clone --branch  R4.4.3 https://github.com/rdkcentral/ThunderTools.git
+
+git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
+
+git clone --branch develop https://github.com/rdkcentral/entservices-apis.git
+
+git clone https://$GITHUB_TOKEN@github.com/rdkcentral/entservices-testframework.git
+
+############################
+# Build Thunder-Tools
+echo "======================================================================================"
+echo "buliding thunderTools"
+cd ThunderTools
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/00010-R4.4-Add-support-for-project-dir.patch
+cd -
+
+
+cmake -G Ninja -S ThunderTools -B build/ThunderTools \
+    -DEXCEPTIONS_ENABLE=ON \
+    -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
+    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+    -DGENERIC_CMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+
+cmake --build build/ThunderTools --target install
+
+
+############################
+# Build Thunder
+echo "======================================================================================"
+echo "buliding thunder"
+
+cd Thunder
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/Use_Legact_Alt_Based_On_ThunderTools_R4.4.3.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/error_code_R4_4.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/1004-Add-support-for-project-dir.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/RDKEMW-733-Add-ENTOS-IDS.patch
+cd -
+
+cmake -G Ninja -S Thunder -B build/Thunder \
+    -DMESSAGING=ON \
+    -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
+    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+    -DGENERIC_CMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+    -DBUILD_TYPE=Debug \
+    -DBINDING=127.0.0.1 \
+    -DPORT=55555 \
+    -DEXCEPTIONS_ENABLE=ON \
+
+cmake --build build/Thunder --target install
+
+
+############################
+# Build entservices-apis
+echo "======================================================================================"
+echo "buliding entservices-apis"
+cd entservices-apis
+rm -rf jsonrpc/DTV.json
+cd ..
+
+cmake -G Ninja -S entservices-apis  -B build/entservices-apis \
+    -DEXCEPTIONS_ENABLE=ON \
+    -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
+    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+
+cmake --build build/entservices-apis --target install
+
+
+
+############################
+# generating extrnal headers
+cd $GITHUB_WORKSPACE
+cd entservices-testframework/Tests
+echo " Empty mocks creation to avoid compilation errors"
+echo "======================================================================================"
+mkdir -p headers
+mkdir -p headers/rdk/ds
+mkdir -p headers/rdk/iarmbus
+mkdir -p headers/ccec/drivers
+mkdir -p headers/ccec/host
+mkdir -p headers/websocket
+echo "dir created successfully"
+echo "======================================================================================"
+
+echo "======================================================================================"
+echo "empty headers creation"
+cd headers
+touch ccec/drivers/CecIARMBusMgr.h
+touch ccec/FrameListener.hpp
+touch ccec/Connection.hpp
+touch ccec/CCEC.hpp
+touch ccec/Assert.hpp
+touch ccec/Messages.hpp
+touch ccec/MessageDecoder.hpp
+touch ccec/MessageProcessor.hpp
+touch ccec/CECFrame.hpp
+touch ccec/MessageEncoder.hpp
+touch ccec/host/RDK.hpp
+touch websocket/URL.h
+touch rdk/ds/dsDisplay.h
+touch rdk/ds/dsError.h
+touch rdk/ds/exception.hpp
+touch rdk/ds/hdmiIn.hpp
+touch rdk/ds/host.hpp
+touch rdk/ds/manager.hpp
+touch rdk/ds/videoOutputPort.hpp
+touch rdk/iarmbus/libIARM.h
+touch rdk/iarmbus/libIBus.h
+touch rfcapi.h
+echo "files created successfully"
+echo "======================================================================================"
+
+cd ../../
+
+ls -la ${GITHUB_WORKSPACE}
